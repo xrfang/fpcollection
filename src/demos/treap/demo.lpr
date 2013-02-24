8b21f43eaff30a46acc3d6fa48f89819fc3b0504
Xiangrong Fang <xrfang@gmail.com>
2013-02-24 13:45:14 +0800

    Added TTreap

---
+++ src/demos/treap/demo.lpr
@@ -0,0 +1,78 @@
+program demo;
+{$mode objfpc}
+uses sysutils, treap;
+type
+
+  { THistogram }
+
+  THistogram = class(specialize TTreap<Char, Integer>)
+  protected
+    function Traverse(Key: Char; Value: Integer): Boolean; override;
+    procedure Dispose(Value: Integer); override;
+  public
+    MKey: Char;
+    Max: Integer;
+    constructor Create; override;
+  end;
+var
+  i : Integer;
+  tp : THistogram;
+  n: THistogram.PNode;
+
+{ THistogram }
+
+function THistogram.Traverse(Key: Char; Value: Integer): Boolean;
+begin
+  if Value > Max then begin
+    MKey := Key;
+    Max := Value;
+  end;
+  Write(Format('%s=%d'#9, [Key, Value]));
+  Result := True;
+end;
+
+procedure THistogram.Dispose(Value: Integer);
+begin
+  WriteLn('Now disposing: ' + IntToStr(Value));
+end;
+
+constructor THistogram.Create;
+begin
+  inherited Create;
+  Max := -1;
+end;
+
+{$R *.res}
+
+begin
+  tp := THistogram.Create;
+  for i := Ord('A') to Ord('Z') do begin
+    tp.Insert(Chr(i), Random(100));
+  end;
+  WriteLn('# of items: ' + IntToStr(tp.Count));
+  tp.Walk;
+  WriteLn;
+  WriteLn(Format('Max=%d at %s', [tp.Max, tp.MKey]));
+  n := tp.Find('X');
+  if n = nil then
+    WriteLn('X is Not Found')
+  else
+    WriteLn('X is ' + IntToStr(n^.Value));
+  WriteLn('x is ' + BoolToStr(tp.Delete('x'), 'Deleted', 'Not Found'));
+  WriteLn('# of items: ' + IntToStr(tp.Count));
+  WriteLn('X is ' + BoolToStr(tp.Delete('X'), 'Deleted', 'Not Found'));
+  WriteLn('# of items: ' + IntToStr(tp.Count));
+  WriteLn('A is ' + BoolToStr(tp.Insert('A', -1), 'Updated', 'Not Changed'));
+  WriteLn('# of items: ' + IntToStr(tp.Count));
+  WriteLn('a is ' + BoolToStr(tp.Insert('a', 65536), 'Updated', 'Not Changed'));
+  WriteLn('# of items: ' + IntToStr(tp.Count));
+  WriteLn('a is ' + BoolToStr(tp.Insert('a', 65536), 'Updated', 'Not Changed'));
+  WriteLn('# of items: ' + IntToStr(tp.Count));
+  tp.Max := -1;
+  tp.Walk(1);
+  WriteLn;
+  WriteLn(Format('Max=%d at %s', [tp.Max, tp.MKey]));
+  tp.Clear;
+  WriteLn(Format('After Clear(), # of items=%d', [tp.Count]));
+  tp.Free;
+end.
---
+++ src/units/treap.pas
@@ -0,0 +1,231 @@
+unit treap;
+{$mode objfpc}
+interface
+type
+
+  { TTreap }
+
+  generic TTreap<TKey, TValue> = class
+    const
+      MAX_PRIORITY = $7FFFFFFF;
+    type
+      Traverser = function(Key: Tkey; Value: TValue): Boolean of object;
+      PNode = ^TNode;
+      TNode = record
+        Key: TKey;
+        Value: TValue;
+        Priority: Cardinal;
+        Left, Right: PNode;
+      end;
+    private
+      Altered: Boolean; //for Insert & Delete
+      GoOn: Boolean;    //for Traverse
+      FCount: Integer;
+      NullNode, RootNode: PNode;
+      function GetCount: Integer;
+      function LeftRotate(Node: PNode): PNode;
+      function RightRotate(Node: PNode): PNode;
+      function InsertNode(Key: TKey; Value: TValue; Node: PNode): PNode;
+      function DeleteNode(Key: TKey; Node: PNode): PNode;
+      procedure TraverseNode(Node: PNode; dir: Integer);
+      procedure ClearNode(Node: PNode);
+      function Compare(Key: TKey; Node: PNode): Integer;
+    protected
+      function Traverse(Key: TKey; Value: TValue): Boolean; virtual;
+      procedure Dispose(Value: TValue); virtual;
+    public
+      property Count: Integer read GetCount;
+      function Insert(Key: TKey; Value: TValue): Boolean;
+      function Delete(Key: TKey): Boolean;
+      function Find(Key: TKey): PNode;
+      procedure Clear;
+      constructor Create; virtual;
+      destructor Destroy; override;
+      procedure Walk(dir: Integer = 0); //default Walk Ascendingly
+  end;
+
+implementation
+
+function TTreap.InsertNode(Key: TKey; Value: TValue; Node: PNode): PNode;
+begin
+  if Node = NullNode then begin
+    New(Node);
+    Node^.Key := Key;
+    Node^.Value := Value;
+    Node^.Priority := Random(MAX_PRIORITY);
+    Node^.Left := NullNode;
+    Node^.Right := NullNode;
+    Altered := True;
+    Inc(FCount);
+  end else begin
+    case Compare(Key, Node) of
+      0: begin
+        Altered := Node^.Value <> Value;
+        Node^.Value := Value;
+      end;
+      1: begin
+        Node^.Right := InsertNode(Key, Value, Node^.Right);
+        if Node^.Right^.Priority < Node^.Priority then
+          Node := RightRotate(Node);
+      end;
+      else begin
+        Node^.Left := InsertNode(Key, Value, Node^.Left);
+        if Node^.Left^.Priority < Node^.Priority then
+          Node := LeftRotate(Node);
+      end;
+    end;
+  end;
+  Result := Node;
+end;
+
+function TTreap.DeleteNode(Key: TKey; Node: PNode): PNode;
+begin
+  if Node <> NullNode then begin
+    case Compare(Key, Node) of
+      0: begin
+        if Node^.Left^.Priority < Node^.Right^.Priority then
+          Node := LeftRotate(Node)
+        else
+          Node := RightRotate(Node);
+        if Node <> NullNode then
+          Node := DeleteNode(Key, Node)
+        else begin
+          Dispose(Node^.Left^.Value);
+          System.Dispose(Node^.Left);
+          Node^.Left := NullNode;
+          Altered := True;
+          Dec(FCount);
+        end;
+      end;
+      1: Node^.Right := DeleteNode(Key, Node^.Right);
+      else Node^.Left := DeleteNode(Key, Node^.Left);
+    end;
+  end;
+  Result := Node;
+end;
+
+procedure TTreap.TraverseNode(Node: PNode; dir: Integer);
+begin
+  if (not GoOn) or (Node = NullNode) then Exit;
+  if dir = 0 then begin
+    TraverseNode(Node^.Left, dir);
+    if GoOn then GoOn := Traverse(Node^.Key, Node^.Value);
+    TraverseNode(Node^.Right, dir);
+  end else begin
+    TraverseNode(Node^.Right, dir);
+    if GoOn then GoOn := Traverse(Node^.Key, Node^.Value);
+    TraverseNode(Node^.Left, dir);
+  end;
+end;
+
+procedure TTreap.ClearNode(Node: PNode);
+begin
+  if Node^.Left <> NullNode then begin
+    ClearNode(Node^.Left);
+    Node^.Left := NullNode;
+  end;
+  if Node^.Right <> NullNode then begin
+    ClearNode(Node^.Right);
+    Node^.Right := NullNode;
+  end;
+  DeleteNode(Node^.Key, Node);
+end;
+
+function TTreap.LeftRotate(Node: PNode): PNode;
+begin
+  Result := Node^.Left;
+  Node^.Left := Result^.Right;
+  Result^.Right := Node;
+end;
+
+function TTreap.GetCount: Integer;
+begin
+  Result := FCount;
+end;
+
+function TTreap.RightRotate(Node: PNode): PNode;
+begin
+  Result := Node^.Right;
+  Node^.Right := Result^.Left;
+  Result^.Left := Node;
+end;
+
+constructor TTreap.Create;
+begin
+  Randomize;
+  New(NullNode);
+  NullNode^.Left := NullNode;
+  NullNode^.Right := NullNode;
+  NullNode^.Priority := MAX_PRIORITY;
+  RootNode := NullNode;
+  FCount := 0;
+end;
+
+destructor TTreap.Destroy;
+begin
+  Clear;
+end;
+
+procedure TTreap.Walk(dir: Integer);
+begin
+  GoOn := True;
+  TraverseNode(RootNode, dir);
+end;
+
+function TTreap.Insert(Key: TKey; Value: TValue): Boolean;
+begin
+  Altered := False;
+  RootNode := InsertNode(Key, Value, RootNode);
+  Result := Altered;
+end;
+
+function TTreap.Delete(Key: TKey): Boolean;
+begin
+  Altered := False;
+  RootNode := DeleteNode(Key, RootNode);
+  Result := Altered;
+end;
+
+function TTreap.Find(Key: TKey): PNode;
+var
+  cmp : Integer;
+begin
+  Result := RootNode;
+  while Result <> NullNode do begin
+    cmp := Compare(Key, Result);
+    if cmp < 0 then
+      Result := Result^.Left
+    else if cmp > 0 then
+      Result := Result^.Right
+    else
+      Break;
+  end;
+  if Result = NullNode then Result := nil;
+end;
+
+procedure TTreap.Clear;
+begin
+  ClearNode(RootNode);
+  RootNode := NullNode;
+end;
+
+function TTreap.Compare(Key: TKey; Node: PNode): Integer;
+begin
+  if Key < Node^.Key then
+    Result := -1
+  else if Key > Node^.Key then
+    Result := 1
+  else
+    Result := 0;
+end;
+
+function TTreap.Traverse(Key: TKey; Value: TValue): Boolean;
+begin
+  Result := True;
+end;
+
+procedure TTreap.Dispose(Value: TValue);
+begin
+end;
+
+end.
