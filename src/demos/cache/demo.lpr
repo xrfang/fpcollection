aefa04f0374c94298facd8d57d4a25dafe10b753
xrfang <xrfang@gmail.com>
2014-11-09 21:45:39 +0800

    TCache: initial implementation

--- src/demos/cache/demo.lpr
+++ src/demos/cache/demo.lpr
@@ -5,30 +5,43 @@ const
 var
   h: TCache;
   p: PChar;
+  v: Pointer;
   b: Boolean;
   t: TDateTime;
-  i, j, fpc: PtrUInt;
+  i, j, hit: PtrUInt;
   key: array[0..4] of PtrUInt;
 begin
+  SetHeapTraceOutput('heap.trc');
   p := 'Hello';
-  h := TCache.Create(20);
-  WriteLn('Size=', h.Size);
-  b := h.Add(p, strlen(p), p) <> nil;
+  h := TCache.Create(20, 2);
+  WriteLn('Width=', h.Width, ', Depth=', h.Depth, ', Capacity=', h.Capacity);
+  b := h.Get(p, strlen(p), v);
   WriteLn(BoolToStr(b, True));
-  WriteLn(BoolToStr(h.Get(p, strlen(p)) <> nil, True));
-  b := h.Add(p, strlen(p), p) <> nil;
-  WriteLn(BoolToStr(b, True));
+  h.Add(p, strlen(p), Pointer(123));
+  WriteLn(BoolToStr(h.Get(p, strlen(p), v), True));
+  WriteLn(QWord(v));
   h.Clear;
-  fpc := 0;
+  b := h.Get(p, strlen(p), v);
+  WriteLn(BoolToStr(b, True));
   t := Now;
   for i := 0 to ITEM_COUNT - 1 do begin
     for j := 0 to 4 do key[j] := $deadbeef + i + j;
-    if h.Add(@key, SizeOf(PtrUInt) * 5, Pointer(i)) <> nil then Inc(fpc);
+    h.Add(@key, SizeOf(PtrUInt) * 5, Pointer($deadbeef + i));
   end;
   t := Now - t;
-  WriteLn('Item Count: ', h.Count);
-  WriteLn('Speed (Item/Sec): ', (ITEM_COUNT/t/86400):0:0);
-  WriteLn('Collision (%): ', (fpc / ITEM_COUNT * 100):0:3);
+  if t > 0 then
+    WriteLn('Write Speed (Item/Sec): ', (ITEM_COUNT/t/86400):0:0);
+  WriteLn('Load Factor (%): ', (h.LoadFactor * 100):0:3);
+  hit := 0;
+  t := Now;
+  for i := 0 to ITEM_COUNT - 1 do begin
+    for j := 0 to 4 do key[j] := $deadbeef + i + j;
+    if h.Get(@key, SizeOf(PtrUInt) * 5, v) then Inc(hit);
+  end;
+  t := Now - t;
+  if t > 0 then
+    WriteLn('Read Speed (Item/Sec): ', (ITEM_COUNT/t/86400):0:0);
+  WriteLn('Hit Rate (%): ', (hit / ITEM_COUNT * 100):0:3);
   h.Free;
 end.
 
--- src/units/cache.pas
+++ src/units/cache.pas
@@ -6,27 +6,29 @@ uses crc;
 type
   PCacheItem = ^TCacheItem;
   TCacheItem = record
-    val: Pointer;
     key: Pointer;
-    len: Cardinal;
+    val: Pointer;
   end;
   TCache = class
   private
+    N: QWord;
     FWidth: Byte;
     FDepth: Byte;
     FSize: LongWord;
     FBuckets: array of Pointer;
     function BucketOf(key: Pointer; keylen: Cardinal): LongWord;
     function GetCapacity: QWord;
+    function GetLoadFactor: Double;
   protected
-    procedure Disposer(ptr: Pointer);
+    procedure Disposer({%H-}ptr: Pointer);
   public
     property Width: Byte read FWidth;
     property Depth: Byte read FDepth;
     property Capacity: QWord read GetCapacity;
+    property LoadFactor: Double read GetLoadFactor;
     constructor Create(AWidth, ADepth: Byte);
     destructor Destroy; override;
-    function Get(key: Pointer; keylen: Cardinal): Pointer;
+    function Get(key: Pointer; keylen: Cardinal; out data: Pointer): Boolean;
     procedure Add(key: Pointer; keylen: Cardinal; val: Pointer);
     procedure Clear;
   end;
@@ -43,6 +45,11 @@ begin
   Result := FSize * FDepth;
 end;
 
+function TCache.GetLoadFactor: Double;
+begin
+  Result := N / FSize / FDepth;
+end;
+
 procedure TCache.Disposer(ptr: Pointer); inline;
 begin
   (* empty *)
@@ -56,33 +63,66 @@ begin
   if ADepth = 0 then FDepth := 1 else FDepth := ADepth;
   FSize := 1 shl FWidth;
   SetLength(FBuckets, FSize);
+  N := 0;
 end;
 
 destructor TCache.Destroy;
 begin
+  Clear;
   FBuckets := nil;
 end;
 
-function TCache.Get(key: Pointer; keylen: Cardinal): Pointer;
+function TCache.Get(key: Pointer; keylen: Cardinal; out data: Pointer): Boolean;
 var
   idx: LongWord;
+  buf: Pointer;
+  i: Integer;
+  ci: PCacheItem;
 begin
+  Result := False;
+  data := nil;
   idx := BucketOf(key, keylen);
-  Result := FBuckets[idx];
+  buf := FBuckets[idx];
+  if buf <> nil then for i := 0 to FDepth - 1 do begin
+    ci := PCacheItem((buf + SizeOf(Pointer) * i)^);
+    if ci = nil then Continue;
+    if CompareByte(key^, ci^.key^, keylen) = 0 then begin
+      data := ci^.val;
+      Exit(True);
+    end;
+  end;
 end;
 
 procedure TCache.Add(key: Pointer; keylen: Cardinal; val: Pointer);
 var
   idx: LongWord;
-  buf: Pointer;
+  buf, slot: Pointer;
+  ci : PCacheItem;
+  i : Byte;
 begin
   idx := BucketOf(key, keylen);
   buf := FBuckets[idx];
   if buf = nil then begin
-    buf := GetMemory(SizeOf(Pointer) * FDepth);
-    FBuckets[idx] := buf;   //<-- WIP
+    buf := AllocMem(SizeOf(Pointer) * FDepth);
+    Pointer(buf^) := GetMem(SizeOf(TCacheItem));
+    FBuckets[idx] := buf;
+    ci := PCacheItem(buf^);
+    Inc(N);
+  end else begin
+    ci := PCacheItem((buf + SizeOf(Pointer) * Random(FDepth))^);
+    for i := 0 to FDepth - 1 do begin
+      slot := buf + SizeOf(Pointer) * i;
+      if Pointer(slot^) = nil then begin
+        Inc(N);
+        Pointer(slot^) := GetMem(SizeOf(TCacheItem));
+        ci := PCacheItem(slot^);
+        Break;
+      end;
+    end;
   end;
-  FBuckets[idx] := val;
+  ci^.key := AllocMem(keylen);
+  Move(key^, ci^.key^, keylen);
+  ci^.val := val;
 end;
 
 procedure TCache.Clear; inline;
@@ -90,16 +130,22 @@ var
   i, j: Integer;
   ci: PCacheItem;
 begin
+  N := 0;
   for i := 0 to FSize - 1 do if FBuckets[i] <> nil then begin
     for j := 0 to FDepth - 1 do begin
-      ci := PCacheItem(FBuckets[i] + j * SizeOf(Pointer));
-      Disposer(ci^.val);
-      Dispose(ci);
+      ci := PCacheItem((FBuckets[i] + j * SizeOf(Pointer))^);
+      if ci <> nil then begin
+        Freemem(ci^.key);
+        Disposer(ci^.val);
+        Dispose(ci);
+      end;
     end;
     FreeMemory(FBuckets[i]);
   end;
   FillChar(FBuckets[0], FSize * SizeOf(Pointer), 0);
 end;
 
+initialization
+Randomize;
 end.
 
