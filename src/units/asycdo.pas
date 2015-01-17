c0e55718465ee27d50bfae4b3979c8b94fc802a8
xrfang <xrfang@gmail.com>
2015-01-17 22:04:54 +0800

    TAsyncDo: initial construction (WIP)

--- src/units/asycdo.pas
+++ src/units/asycdo.pas
@@ -7,22 +7,53 @@ uses Classes {, SysUtils};
 type
   TAsyncDo = class
   private type
+    TaskHandler = procedure(input: PtrUInt; output: Pointer) of object;
     Worker = class(TThread)
     private
       Signal: PRTLEvent;
+      WorkLoad: PtrUInt;
+      Handler: TaskHandler;
+      IPtr: PtrUInt;
+      OPtr: Pointer;
     public
-      constructor Create(const StackSize: SizeUInt = DefaultStackSize);
+      procedure Execute; override;
+      constructor Create(const StackSize: SizeUInt);
     end;
   private
     NumWorker: Integer;
     wks: array of Worker;
-
+  protected
+    procedure Task(input: PtrUInt; output: Pointer = nil); virtual;
   public
     constructor Create(workers: Integer; stack: Integer = DefaultStackSize);
     destructor Destroy; override;
   end;
 
 implementation
+
+procedure TAsyncDo.Worker.Execute;
+begin
+  while not Terminated do begin
+    RTLeventWaitFor(Signal);
+    if Terminated then Exit;
+    RTLeventResetEvent(Signal);
+    Handler
+    if not IsPrime then Number := 0;
+    Application.QueueAsyncCall(@AddPrime, PtrInt(Self));
+  end;
+end;
+
+constructor TAsyncDo.Worker.Create(const StackSize: SizeUInt);
+begin
+  inherited Create(False, StackSize);
+  RTLeventSetEvent(Signal);
+end;
+
+procedure TAsyncDo.Task(input: PtrUInt; output: Pointer);
+begin
+  (* empty *)
+end;
+
 constructor TAsyncDo.Create(workers: Integer; stack: Integer);
 var
   i: Integer;
@@ -30,8 +61,8 @@ begin
   NumWorker := workers;
   SetLength(wks, workers);
   for i := 0 to workers - 1 do begin
-    wks[i] := Worker.Create(False, stack);
-    RTLeventSetEvent(wks[i].Signal);
+    wks[i] := Worker.Create(stack);
+    wks[i].Handler := @Task;
   end;
 end;
 
